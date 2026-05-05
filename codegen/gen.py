d918457bd096b1697c1e7aed56d8d05a69c1c548
Sergei Kozelko <kozelkos@yandex.ru>
2026-05-05 21:43:25 +0800

    Generate attributes for multi-field and nested arrays
    Tested against xivapi and exd.camora.dev

--- codegen/gen.py
+++ codegen/gen.py
@@ -1,11 +1,38 @@
 import os
+import sys
 import yaml
 import typing
 from pathlib import Path
 
+
+class RowWriter:
+    field_idx: int = 0
+
+    def __init__(self, file):
+        self.file = file
+
+    def write_class_header(self, name):
+        print(f"class {name}Row(ExdRow):", file=self.file)
+
+    def write_display_field(self, display_field):
+        print(f"    _display_field: str = '{display_field}'", file=self.file)
+        print(f"", file=self.file)
+
+    def write_field(self, name):
+        print(f"    {name}: typing.Any = {self.field_idx}", file=self.file)
+        self.field_idx += 1
+
+
 def __main__() -> typing.Any:
     schema_dir = Path(__file__).parent / "EXDSchema"
-    rowdef_path = Path(__file__).parent.parent / "src" / "pyxivdata" / "resource" / "excel" / "rowdef.py"
+    rowdef_path = (
+        Path(__file__).parent.parent
+        / "src"
+        / "pyxivdata"
+        / "resource"
+        / "excel"
+        / "rowdef.py"
+    )
     with open(rowdef_path, "w") as rowdef:
         print("from pyxivdata.resource.excel.reader import ExdRow", file=rowdef)
         print("import typing", file=rowdef)
@@ -14,26 +41,37 @@ def __main__() -> typing.Any:
         for path in schema_dir.glob("*.yml"):
             print(path)
             with open(path, "r") as file:
+                writer = RowWriter(rowdef)
                 yml = yaml.safe_load(file)
-                print(f"class {yml["name"]}Row(ExdRow):", file=rowdef)
+
+                writer.write_class_header(yml["name"])
 
                 if "displayField" in yml:
-                    print(f"    _display_field: str = '{yml["displayField"]}'", file=rowdef)
-                    print(f"", file=rowdef)
+                    writer.write_display_field(yml["displayField"])
 
-                idx = 0
                 for field in yml["fields"]:
-                    if field.get("type") == "array":
-                        for sub_idx in range(field["count"]):
-                            print(f"    {field["name"]}{sub_idx}: typing.Any = {idx}", file=rowdef)
-                            idx += 1
-                    else:
-                        print(f"    {field["name"]}: typing.Any = {idx}", file=rowdef)
-                        idx += 1
+                    write_field(field, writer)
 
                 print(f"", file=rowdef)
 
     return 1
+
+
+def write_field(field, writer: RowWriter, name_prefix: str = ""):
+    field_name = name_prefix + field.get("name", "")
+
+    if len(field_name) == 0:
+        print("Empty field name", file=sys.stderr)
+
+    if field.get("type") == "array":
+        for sub_idx in range(field["count"]):
+            if "fields" in field:
+                for sub_field in field["fields"]:
+                    write_field(sub_field, writer, name_prefix=f"{field_name}{sub_idx}")
+            else:
+                writer.write_field(f"{field_name}{sub_idx}")
+    else:
+        writer.write_field(field_name)
 
 
 if __name__ == "__main__":
